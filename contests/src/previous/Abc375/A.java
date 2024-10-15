8a4f41cd794077193f5684c3dfad0ec57a107518
jjagrit <jjagrit@cisco.com>
2024-10-15 08:24:09 +0530

    Atcoder Contest

---
+++ contests/src/previous/Abc375/A.java
@@ -0,0 +1,21 @@
+package previous.Abc375;
+
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s = sc.next();
+        int count = 0;
+
+        for(int i=1;i<n-1;i++) {
+            if(s.charAt(i)=='.' && s.charAt(i-1)=='#' && s.charAt(i+1)=='#') {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
+
---
+++ contests/src/previous/Abc375/B.java
@@ -0,0 +1,32 @@
+package previous.Abc375;
+
+import java.util.Scanner;
+
+public class B {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        double points[][] = new double[n+2][2];
+        points[0][0] = 0;
+        points[0][1] = 0;
+        points[n+1][0] = 0;
+        points[n+1][1] = 0;
+        for(int i=1;i<=n;i++) {
+            points[i][0] = sc.nextDouble();
+            points[i][1] = sc.nextDouble();
+        }
+
+        double distance = 0;
+
+        for(int i=1;i<points.length;i++) {
+            double x = Math.pow((points[i][0] - points[i-1][0]),2);
+            double y = Math.pow((points[i][1]- points[i-1][1]), 2);
+            double sq = Math.sqrt(x+y);
+//            System.out.println(sq);
+            distance += sq;
+        }
+
+        System.out.println(distance);
+    }
+}
+
